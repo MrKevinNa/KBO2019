fcb49cc4622fd5d72a8671462c499f506cc1fe76
MrKevinNa <drkevin2022@gmail.com>
2020-11-28 10:15:50 +0900

    add files

---
+++ R/hitters.R
@@ -0,0 +1,13 @@
+#' KBO Hitters' stats in 2019
+#'
+#' 300 Players from 10 teams have 17 stats.
+#'
+#' @source <http://www.kbreport.com/leader/main>
+#' @format Tibble with columns
+#' \describe{
+#' \item{선수명}{Player Name}
+#' \item{팀명}{Team Name}
+#' }
+#' @examples
+#'   hitters
+"hitters"
